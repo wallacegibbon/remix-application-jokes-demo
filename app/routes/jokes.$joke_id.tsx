248e028c39723659168e2d9761b2310a8fde6691
wallacegibbon <wallacegibbon@aliyun.com>
2023-08-15 01:01:25 +0800

    Trivial progress: Sample code to use model's type declarations from Prisma.

--- app/routes/jokes.$joke_id.tsx
+++ app/routes/jokes.$joke_id.tsx
@@ -1,15 +1,21 @@
-import {useLoaderData} from "@remix-run/react";
-import {LoaderFunction} from "react-router";
+import { Link, useLoaderData } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/node";
+import { Joke } from "@prisma/client";
+import { db } from "~/util/db.server";
 
 export var loader: LoaderFunction = async function ({ params }) {
-	console.log(">>>", params);
-	return "hello";
+	var joke = await db.joke.findUnique({ where: { id: params.joke_id }});
+	if (!joke)
+		throw new Error("joke not found");
+	return json<Joke>(joke);
 };
 
 function JokeIdRoute() {
-	var data = useLoaderData();
+	var joke = useLoaderData<Joke>();
 	return <div>
-		<div>{JSON.stringify(data)}</div>
+		<p>Here's your hilarious joke:</p>
+		<p>{joke.content}</p>
+		<Link to=".">"{joke.name}" Permalink</Link>
 	</div>;
 }
 
--- app/routes/jokes._index.tsx
+++ app/routes/jokes._index.tsx
@@ -1,9 +1,21 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { Joke } from "@prisma/client";
+import { db } from "~/util/db.server";
+
+export var loader: LoaderFunction = async function () {
+	var count = await db.joke.count();
+	var random_num = Math.floor(Math.random() * count);
+	var [ joke ] = await db.joke.findMany({ skip: random_num, take: 1 });
+	return json<Joke>(joke);
+};
+
 function JokesIndexRoute() {
+	var joke = useLoaderData<Joke>();
 	return <div>
 		<p>Here's a random joke:</p>
-		<p>
-			I was wondering why the frisbee was getting bigger, then it hit me.
-		</p>
+		<p>{joke.content}</p>
+		<Link to={joke.id}>"{joke.name}" Permalink</Link>
 	</div>;
 }
 
